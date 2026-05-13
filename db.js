a8bcea90f60f3c20913ba1be6b1947ebbcade840
rikelmepereira01-cell <rikelme.pereira01@aluno.unifametro.edu.br>
2026-05-13 18:46:55 -0300

    reconfig

--- db.js
+++ db.js
@@ -1,8 +1,4 @@
 const mysql = require('mysql2/promise');
-require('dotenv').config();
-
-console.log('>>> DB_HOST:', process.env.DB_HOST);
-console.log('>>> DB_PORT:', process.env.DB_PORT);
 
 const pool = mysql.createPool({
   host:     process.env.DB_HOST,
@@ -10,9 +6,7 @@ const pool = mysql.createPool({
   user:     process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  ssl: {
-    rejectUnauthorized: false
-  },
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
 });
